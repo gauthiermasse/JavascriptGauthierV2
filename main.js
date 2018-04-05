02d705602af90019feff9d6ceda63ffabf56642f
Gauthier Masse <masse.gauthier@gmail.com>
2018-04-05 22:07:30 +0200

    retour fichiers

---
+++ main.js
@@ -0,0 +1,5 @@
+function Fonction1() {
+	document.getElementById('tuss').style.display='none';
+}
+
+setTimeout(function(){alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000)
